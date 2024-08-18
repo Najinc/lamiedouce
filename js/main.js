d6277b65c1d867c9d65997744afc2e9117b31ed2
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-08-18 17:12:31 +0200

    edit: index.html, style.css et main.js
    Ajout des media queries pour la navbar

--- js/main.js
+++ js/main.js
@@ -1,44 +1,10 @@
-// Carrousel
-document.addEventListener('DOMContentLoaded', function() {
-    const carousel = document.getElementById('default-carousel');
-    const items = carousel.querySelectorAll('[data-carousel-item]');
-    const indicators = carousel.querySelectorAll('[data-carousel-slide-to]');
-    const prevButton = carousel.querySelector('[data-carousel-prev]');
-    const nextButton = carousel.querySelector('[data-carousel-next]');
-    let currentIndex = 0;
+// Navbar
+const burgerButton = document.querySelector('[aria-controls="mobile-menu"]');
+const mobileMenu = document.getElementById('mobile-menu');
 
-    function showSlide(index) {
-        items.forEach((item, i) => {
-            item.classList.toggle('hidden', i !== index);
-        });
-        indicators.forEach((indicator, i) => {
-            indicator.setAttribute('aria-current', i === index);
-        });
-    }
+burgerButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
 
-    function showNextSlide() {
-        currentIndex = (currentIndex + 1) % items.length;
-        showSlide(currentIndex);
-    }
-
-    function showPrevSlide() {
-        currentIndex = (currentIndex - 1 + items.length) % items.length;
-        showSlide(currentIndex);
-    }
-
-    indicators.forEach((indicator, index) => {
-        indicator.addEventListener('click', () => {
-            currentIndex = index;
-            showSlide(currentIndex);
-        });
-    });
-
-    nextButton.addEventListener('click', showNextSlide);
-    prevButton.addEventListener('click', showPrevSlide);
-
-    // Auto slide
-    setInterval(showNextSlide, 5000); // Change every 5 seconds
-
-    // Initialize first slide
-    showSlide(currentIndex);
+    burgerButton.querySelector('svg:nth-child(1)').classList.toggle('hidden');
+    burgerButton.querySelector('svg:nth-child(2)').classList.toggle('hidden');
 });
