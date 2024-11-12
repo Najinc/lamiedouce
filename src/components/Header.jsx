e4d53f5de6b09fa5f706f5eb6d9c2a919044c012
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-11-13 00:16:26 +0100

    fix: repair mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Header() {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     return (
         <nav className="bg-middle-purple" id="#top">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -48,46 +54,30 @@ export default function Header() {
                             type="button"
                             className="relative inline-flex items-center justify-center rounded-md bg-dark-purple hover:bg-light-purple p-2 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                             aria-controls="mobile-menu"
-                            aria-expanded="false"
+                            aria-expanded={isMobileMenuOpen}
+                            onClick={toggleMobileMenu}
                         >
                             <span className="absolute -inset-0.5" />
                             <span className="sr-only">Open main menu</span>
 
-                            <svg
-                                className="block h-6 w-6"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                                />
-                            </svg>
+                            <i
+                                className={`bx bx-menu text-white ${isMobileMenuOpen ? "hidden" : "block"}`}
+                                style={{ fontSize: "24px" }}
+                            />
 
-                            <svg
-                                className="hidden h-6 w-6"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
-                            </svg>
+                            <i
+                                className={`bx bx-x text-white ${isMobileMenuOpen ? "block" : "hidden"}`}
+                                style={{ fontSize: "24px" }}
+                            />
                         </button>
                     </div>
                 </div>
             </div>
 
-            <div className="hidden" id="mobile-menu">
+            <div
+                className={`${isMobileMenuOpen ? "block" : "hidden"}`}
+                id="mobile-menu"
+            >
                 <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
                     <a
                         href="/"
